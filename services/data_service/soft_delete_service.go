2ccec3a52a71f4ff9296eac71623cd800139e887
chiquesricardochiques-sys <chiquesricardochiques@gmail.com>
2026-01-15 22:49:40 -0300

    Update soft_delete_service.go

--- services/data_service/soft_delete_service.go
+++ services/data_service/soft_delete_service.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"time"
 
+	"meu-provedor/models"
 	"meu-provedor/config"
 	"meu-provedor/engine/query"
 )
 
 // ExecuteSoftDelete executa soft delete (UPDATE deleted_at)
-func ExecuteSoftDelete(req DeleteRequest) (int64, error) {
+func ExecuteSoftDelete(req models.DeleteRequest) (int64, error) {
 	projectCode, err := getProjectCodeByID(req.ProjectID)
 	if err != nil {
 		return 0, err
@@ -71,3 +72,4 @@ func ensureSoftDeleteColumn(db *sql.DB, table string) error {
 
 	return nil
 }
+
