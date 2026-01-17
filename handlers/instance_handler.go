e0c768e38509826f4c5cb6c5af21558dea636555
chiquesricardochiques-sys <chiquesricardochiques@gmail.com>
2026-01-17 17:32:51 -0300

    Update instance_handler.go

--- handlers/instance_handler.go
+++ handlers/instance_handler.go
@@ -7,19 +7,20 @@ import (
 
 	"github.com/gorilla/mux"
 
-	"meu-provedor/services/instance"
 	"meu-provedor/models"
+	"meu-provedor/services/instance"
 )
 
 func CreateInstance(w http.ResponseWriter, r *http.Request) {
 	var req models.InstanceRequest
+
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, "invalid json", 400)
+		http.Error(w, "invalid json", http.StatusBadRequest)
 		return
 	}
 
 	if err := instance.Create(req); err != nil {
-		http.Error(w, err.Error(), 400)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
@@ -33,7 +34,7 @@ func ListInstances(w http.ResponseWriter, r *http.Request) {
 	if pid := r.URL.Query().Get("project_id"); pid != "" {
 		id, err := strconv.ParseInt(pid, 10, 64)
 		if err != nil {
-			http.Error(w, "invalid project_id", 400)
+			http.Error(w, "invalid project_id", http.StatusBadRequest)
 			return
 		}
 		projectID = &id
@@ -41,21 +42,29 @@ func ListInstances(w http.ResponseWriter, r *http.Request) {
 
 	instances, err := instance.List(projectID)
 	if err != nil {
-		http.Error(w, err.Error(), 500)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(instances)
 }
 
 func UpdateInstance(w http.ResponseWriter, r *http.Request) {
-	id, _ := strconv.ParseInt(mux.Vars(r)["id"], 10, 64)
+	id, err := strconv.ParseInt(mux.Vars(r)["id"], 10, 64)
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
+	}
 
 	var req models.InstanceRequest
-	json.NewDecoder(r.Body).Decode(&req)
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid json", http.StatusBadRequest)
+		return
+	}
 
 	if err := instance.Update(id, req); err != nil {
-		http.Error(w, err.Error(), 400)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
@@ -63,14 +72,16 @@ func UpdateInstance(w http.ResponseWriter, r *http.Request) {
 }
 
 func DeleteInstance(w http.ResponseWriter, r *http.Request) {
-	id, _ := strconv.ParseInt(mux.Vars(r)["id"], 10, 64)
+	id, err := strconv.ParseInt(mux.Vars(r)["id"], 10, 64)
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
+	}
 
 	if err := instance.Delete(id); err != nil {
-		http.Error(w, err.Error(), 400)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	w.Write([]byte("INSTANCE DELETED"))
 }
-
-
