65a45636407cd5948674eea1516b46414e6931a0
chiquesricardochiques-sys <chiquesricardochiques@gmail.com>
2026-01-17 17:30:32 -0300

    Update instance.go

--- models/instance.go
+++ models/instance.go
@@ -3,11 +3,17 @@ package models
 
 type InstanceRequest struct {
 	ProjectID   int64                  `json:"project_id"`
+	ClientName  string                 `json:"client_name"`
+	Email       string                 `json:"email"`
+	Phone       string                 `json:"phone"`
+	Price       float64                `json:"price"`
+	PaymentDay  int                    `json:"payment_day"`
 	Name        string                 `json:"name"`
 	Code        string                 `json:"code"`
 	Description string                 `json:"description"`
 	Status      string                 `json:"status"`
 	Settings    map[string]interface{} `json:"settings"`
 }
+
 
 
