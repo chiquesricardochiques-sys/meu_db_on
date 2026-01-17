17165cafc6b6c6cdf1b20e34174dfac41c8d9346
chiquesricardochiques-sys <chiquesricardochiques@gmail.com>
2026-01-17 10:21:49 -0300

    Update table_handler.go

--- handlers/table_handler.go
+++ handlers/table_handler.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 
 	"meu-provedor/models"
+	
+	"fmt"
+	"strings"
+
 	tableService "meu-provedor/services/table"
 )
 
@@ -51,3 +55,110 @@ func DeleteProjectTable(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("TABLE DELETED"))
 }
+
+
+
+
+// GET TABLE DETAILS
+func GetTableDetails(w http.ResponseWriter, r *http.Request) {
+	projectCode := r.URL.Query().Get("project_code")
+	tableName := r.URL.Query().Get("table")
+	if projectCode == "" || tableName == "" {
+		http.Error(w, "project_code and table are required", 400)
+		return
+	}
+
+	details, err := tableService.GetDetails(projectCode, tableName)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	json.NewEncoder(w).Encode(details)
+}
+
+// ADD COLUMN
+func AddColumn(w http.ResponseWriter, r *http.Request) {
+	projectCode := r.URL.Query().Get("project_code")
+	tableName := r.URL.Query().Get("table")
+
+	var col tableService.ColumnRequest
+	if err := json.NewDecoder(r.Body).Decode(&col); err != nil {
+		http.Error(w, "invalid json", 400)
+		return
+	}
+
+	if err := tableService.AddColumn(projectCode, tableName, col); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	w.Write([]byte("COLUMN ADDED"))
+}
+
+// MODIFY COLUMN
+func ModifyColumn(w http.ResponseWriter, r *http.Request) {
+	projectCode := r.URL.Query().Get("project_code")
+	tableName := r.URL.Query().Get("table")
+
+	var col tableService.ColumnRequest
+	if err := json.NewDecoder(r.Body).Decode(&col); err != nil {
+		http.Error(w, "invalid json", 400)
+		return
+	}
+
+	if err := tableService.ModifyColumn(projectCode, tableName, col); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	w.Write([]byte("COLUMN MODIFIED"))
+}
+
+// DROP COLUMN
+func DropColumn(w http.ResponseWriter, r *http.Request) {
+	projectCode := r.URL.Query().Get("project_code")
+	tableName := r.URL.Query().Get("table")
+	columnName := r.URL.Query().Get("column")
+
+	if err := tableService.DropColumn(projectCode, tableName, columnName); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	w.Write([]byte("COLUMN DROPPED"))
+}
+
+// ADD INDEX
+func AddIndex(w http.ResponseWriter, r *http.Request) {
+	projectCode := r.URL.Query().Get("project_code")
+	tableName := r.URL.Query().Get("table")
+
+	var idx tableService.IndexRequest
+	if err := json.NewDecoder(r.Body).Decode(&idx); err != nil {
+		http.Error(w, "invalid json", 400)
+		return
+	}
+
+	if err := tableService.AddIndex(projectCode, tableName, idx); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	w.Write([]byte("INDEX ADDED"))
+}
+
+// DROP INDEX
+func DropIndex(w http.ResponseWriter, r *http.Request) {
+	projectCode := r.URL.Query().Get("project_code")
+	tableName := r.URL.Query().Get("table")
+	indexName := r.URL.Query().Get("index")
+
+	if err := tableService.DropIndex(projectCode, tableName, indexName); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	w.Write([]byte("INDEX DROPPED"))
+}
+
