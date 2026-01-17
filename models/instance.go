bb351a5f1c9d10c89637be489de85ec784d1a79e
chiquesricardochiques-sys <chiquesricardochiques@gmail.com>
2026-01-17 09:11:02 -0300

    Update instance.go

--- models/instance.go
+++ models/instance.go
@@ -2,16 +2,7 @@ package models
 
 import "time"
 
-type Instance struct {
-	ID          int64                  `json:"id"`
-	ProjectID   int64                  `json:"project_id"`
-	Name        string                 `json:"name"`
-	Code        string                 `json:"code"`
-	Description string                 `json:"description"`
-	Status      string                 `json:"status"`
-	Settings    map[string]interface{} `json:"settings"`
-	CreatedAt   time.Time              `json:"created_at"`
-}
+
 
 
 
@@ -23,3 +14,4 @@ type InstanceRequest struct {
 	Status      string                 `json:"status"`
 	Settings    map[string]interface{} `json:"settings"`
 }
+
